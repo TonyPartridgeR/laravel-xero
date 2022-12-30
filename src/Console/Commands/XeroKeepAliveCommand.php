1b5acd5a4f8ee9a82e9a9a3e89331b03f6295e2a
Tony Partridge <103045669+TonyPartridgeR@users.noreply.github.com>
2022-12-30 14:50:08 +0000

    Ensure Keep Alive only keeps alive tokens with a tenant_id
    Some strange scenarios we get an empty tenant_id so we need to be sure we are only refreshing tokens which have a tenant ID.

--- src/Console/Commands/XeroKeepAliveCommand.php
+++ src/Console/Commands/XeroKeepAliveCommand.php
@@ -14,7 +14,7 @@ class XeroKeepAliveCommand extends Command
     public function handle()
     {
         // Fetch all tenants for when multiple tenants are in use.
-        $tenants = XeroToken::all();
+        $tenants = XeroToken::whereNotNull('tenant_id')->get();
 
         foreach($tenants as $tenant) {
 
